f4df109bc09a0a88b3fe4d670a476a9dec7890f1
Dainel Vera <arubinofaux@gmail.com>
2023-12-08 13:10:54 -0700

    add LibBase64-1.0 for text encoding

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 --the ace way of initializing addons, add the ace modules 
 --you want to use as trailing parameters in :NewAddon()
-EonLash = LibStub("AceAddon-3.0"):NewAddon("EonLash", "AceEvent-3.0", "AceConsole-3.0", "AceHook-3.0")
+EonLash = LibStub("AceAddon-3.0"):NewAddon("EonLash", "AceEvent-3.0", "AceConsole-3.0")
 
 EonLash.scanner = {}
 
@@ -11,3 +11,6 @@ EonLash.prefix = "eonlash";
 
 -- Load AceGUI-3.0
 AceGUI = LibStub("AceGUI-3.0")
+
+-- Load LibBase64-1.0
+EonLash.Base64 = LibStub("LibBase64-1.0")
